e8a8e9b865001b4b1fd4e040593cc29cf05937fd
anhln1998 <anhln@mor.com.vn>
2023-11-02 21:17:02 +0700

    update unit test

--- src/screens/Product.tsx
+++ src/screens/Product.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Slider from "react-slick";
 
 function ProductScreen() {
